b45fc9febfff869e210267117a30198307b118fc
DEnFUrt <musorov@gmail.com>
2019-11-17 20:58:58 +0500

    Доработал кнопку перенсти значения

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,7 @@
     const btnPercent = document.getElementById('btnPercent');
     const btnEsc = document.getElementById('btnEsc');
     const btnChangeValue = document.getElementById('btnChangeValue');
+    const btnUpResult = document.getElementById('btnUpResult');
     const inputFirstValue = document.getElementById('firstValue');
     const inputSecondValue = document.getElementById('secondValue');
     const fieldResult = document.getElementById('result');
@@ -11,7 +12,7 @@
 
     let firstValue = 0;
     let secondValue = 0;
-    let action = '+';
+    let action = '';
 
     setFocus();
     
@@ -28,6 +29,7 @@
         secondValue = 0;
     });
 
+    //проверяем строку ввода первого оператора после потери фокуса ввода, если не число, то сбрасываем значение на 0
     inputFirstValue.addEventListener('change', (e) => {
         if (e.currentTarget.value.length === 0 
             || (!Number(e.currentTarget.value) 
@@ -40,6 +42,7 @@
         }
     });
 
+    ////проверяем строку ввода второго оператора после потери фокуса ввода, если не число, то сбрасываем значение на 0
     inputSecondValue.addEventListener('change', (e) => {
         if (e.currentTarget.value.length === 0 
             || (!Number(e.currentTarget.value) 
@@ -51,20 +54,26 @@
         }
     });
 
+    //Считаем результат, вызываем функцию чистки строк ввода оператора, передаем фокус на первую строку ввода
     btnResult.addEventListener('click', () => {
         console.log(firstValue, secondValue, action);
-
-        fieldResult.innerHTML = calculate(firstValue, secondValue, action);
-        clearValue();
+        if (inputFirstValue.value !== 'не число!' && inputSecondValue.value !== 'не число!') {
+            fieldResult.innerHTML = calculate(firstValue, secondValue, action);
+            if (fieldResult.textContent !== 'нет операнда!') {
+                clearValue();
+            }
+        }
         setFocus();
     });
 
+    //Если нажали кнопку С вызываем функцию чистки строки ввода и результата
     btnEsc.addEventListener('click', () => {
         let clearResult = true;
         clearValue(clearResult);
         setFocus();
     });
 
+    //Если нажата кнопка -/+ меняем знак результата
     btnChangeValue.addEventListener('click', () => {
         if (fieldResult.textContent[0] === '-') {
             fieldResult.textContent = fieldResult.textContent.replace('-', '');
@@ -73,14 +82,21 @@
         }
     });
 
-    for (let i = 0; i < controls.length; i++) {
-        const btn = controls[i];
-        btn.addEventListener('click', (e) => {
-            action = e.currentTarget.innerHTML;
-        });
-    }
+    btnUpResult.addEventListener('click', () => {
+        if (inputFirstValue.value === '' || inputFirstValue.value === '0') {
+            inputFirstValue.value = fieldResult.textContent;
+            firstValue = parseFloat(fieldResult.textContent);
+        } else if (inputSecondValue.value === '' || inputSecondValue.value === '0') {
+            inputSecondValue.value = fieldResult.textContent;
+            secondValue = parseFloat(fieldResult.textContent);
+        }
+    });
 
+    //Ставим фокус ввода на первую строку
     function setFocus () {
+        if (inputFirstValue.value === 'не число!') {
+            inputFirstValue.value = '';
+        }
         inputFirstValue.focus();
     }
 
@@ -93,6 +109,15 @@
         firstValue = 0;
         inputSecondValue.value = '';
         secondValue = 0;
+        action = '';
+    }
+
+    //Определяем какая кнопка операнда была нажата
+    for (let i = 0; i < controls.length; i++) {
+        const btn = controls[i];
+        btn.addEventListener('click', (e) => {
+            action = e.currentTarget.innerHTML;
+        });
     }
 
     function calculate(x, y, action) {
@@ -111,6 +136,8 @@
                     return x / y;
                 }
                 return 'бесконечность';
+            default:
+                return 'нет операнда!';
         }
     }
 })();
